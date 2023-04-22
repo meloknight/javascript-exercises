ccae0635695a6fb853858c574224f47eaa83a925
Cornelius <49502879+meloknight@users.noreply.github.com>
2023-04-22 10:50:06 -0700

    Successfully completed 01 and 02.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(repStr, numOfTimesRepeated) {
+    let outputStr;
+    if (numOfTimesRepeated < 0) {
+        outputStr = "ERROR" 
+    } else {
+        outputStr = "";
+        for (let i = 0; i < numOfTimesRepeated; i++) {
+            outputStr = outputStr + repStr;
+        } 
+    }
 
+    return outputStr;
 };
 
 // Do not edit below this line
