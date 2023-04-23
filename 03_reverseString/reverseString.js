a00d39753366e406292e1c9c88e16fa487a57a37
Cornelius <49502879+meloknight@users.noreply.github.com>
2023-04-23 15:28:49 -0700

    completed the 03 module.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(inputStr) {
+    let reversedStr = "";
+    let tempChar;
+    for (let i = inputStr.length - 1; i >= 0; i--) {
+        tempChar = inputStr[i];
+        reversedStr = reversedStr + tempChar;
 
+        // reversedStr = reversedStr + inputStr[i];
+    }
+
+    return reversedStr;
 };
 
 // Do not edit below this line
