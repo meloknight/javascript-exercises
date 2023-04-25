7235e77b4cbb14f68320ac8d34988c133b5e7f33
Cornelius <49502879+meloknight@users.noreply.github.com>
2023-04-24 20:38:11 -0700

    completed exercise 05 and passed all tests.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let theSum = 0;
+
+    if (Number.isInteger(num1) === false || Number.isInteger(num2) === false) {
+        return "ERROR";
+    };
+
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR"
+    }
+
+    if (num1 >= num2) {
+        [num1, num2] = [num2, num1];
+    }; 
+
+    for (let i = num1; i <= num2; i++) {
+        theSum = theSum + i;
+    };
+
+    return theSum;
 
 };
 
