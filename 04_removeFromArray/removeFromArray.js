7eb3781e8e334cef635850c0deb6b52c88d01caf
Cornelius <49502879+meloknight@users.noreply.github.com>
2023-04-24 20:18:38 -0700

    Completed exercise 04 and passed all tests.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...itemsToRemove) {
 
+    for (let j = 0; j <= itemsToRemove.length - 1; j++) {
+        for (let i = 0; i <= inputArray.length - 1; i++) {
+            if (inputArray[i] === itemsToRemove[j]) {
+                inputArray.splice(i, 1);
+            }
+        }
+    }
+    
+    return inputArray;
 };
 
 // Do not edit below this line
