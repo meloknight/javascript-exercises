1f48812c5fb7f12a0b7ca86ec27eca03c2c209a3
Cornelius <49502879+meloknight@users.noreply.github.com>
2023-04-25 07:14:00 -0700

    completed exercise 07 and passed all tests.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  let convertedToCelsius = (tempInFahrenheit - 32) / 1.8;
+  convertedToCelsius = convertedToCelsius.toFixed(1);
+  convertedToCelsius = parseFloat(convertedToCelsius);
+  return convertedToCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+  let convertedToFahrenheit = (tempInCelsius * 1.8) + 32;
+  convertedToFahrenheit = convertedToFahrenheit.toFixed(1);
+  convertedToFahrenheit = parseFloat(convertedToFahrenheit);
+  return convertedToFahrenheit;
 };
 
 // Do not edit below this line
